d8488d433588f005a586b0eeba2c3dbf595c02f1
Luthando MKHWANAZI <lmkhwana@e5r5p9.wethinkcode.co.za>
2018-11-09 13:24:32 +0200

    WIP

--- config/setup.php
+++ config/setup.php
@@ -2,11 +2,13 @@
     include 'database.php';
 
     try {
+        //Create the camagru database
     $sql = "CREATE DATABASE IF NOT EXISTS camagru";
-    $conn->exec($sql);
+    $db->exec($sql);
     $sql = "use camagru";
-    $conn->exec($sql);
-    $sql = "CREATE TABLE `users` (
+    $db->exec($sql);
+    //create the users table
+    $sql = "CREATE TABLE IF NOT EXISTS `users` (
         `id` int(11) NOT NULL,
         `f_name` varchar(255) NOT NULL,
         `l_name` varchar(255) NOT NULL,
@@ -17,27 +19,27 @@
         `passwordunsecure` varchar(255) NOT NULL,
         `is_confirmed` int(11) NOT NULL DEFAULT '0'
       )";
-    $conn->exec($sql);
-    $sql = "CREATE TABLE `comments` (
+    $db->exec($sql);
+    //create the comments table
+    $sql = "CREATE TABLE IF NOT EXISTS `comments` (
         `id` int(11) NOT NULL,
         `post_id` int(11) NOT NULL,
         `comment` varchar(255) NOT NULL,
         `user` varchar(255) NOT NULL
       )";
-    $conn->exec($sql);
-    $sql = "CREATE TABLE `gallery` (
+    $db->exec($sql);
+    //create the gallery table
+    $sql = "CREATE TABLE IF NOT EXISTS `gallery` (
         `id` int(11) NOT NULL,
         `user` varchar(255) NOT NULL,
         `path` varchar(255) NOT NULL,
         `date_created` datetime NOT NULL
       )";
-    $conn->exec($sql);
-    echo "DB created successfully";
+    $db->exec($sql);
+    header('Location: ../index.php');
     }
     catch(PDOException $e)
     {
-    echo $sql . "<br>" . $e->getMessage();
+        echo $sql . "<br>" . $e->getMessage();
     }
-
-    $conn = null;
 ?>
